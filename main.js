323b094e16199a5f8d68a5545cfc7e07ca62573d
Joseph <jrichardson@jmstudios.net>
2017-10-11 12:18:26 -0700

    some css for looks and more functionality but need to figure out why board only updates after 6 clicks

--- main.js
+++ main.js
@@ -1,20 +1,52 @@
-var initailBoard = [[0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0], 
-                        [0, 0, 0, 0, 0, 0, 0], 
-                        [0, 0, 0, 0, 0, 0, 0], 
-                        [0, 0, 0, 0, 0, 0, 0], 
-                        [0, 0, 0, 0, 0, 0, 0]]
+var initailBoard = [[0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0], 
+                    [0, 0, 0, 0, 0, 0]]
+var gameBoard = initailBoard
 
-function initializeBoard() {
+function goPlayerTwo() {
+    let row = 0
+    while (!colAvailable(row)) {
+        row++
+    }
+    addDisk(2, row)
+}
 
+function goPlayerOne(index) {
+    var col = gameBoard[index]
+    if (colAvailable(index)) {
+        addDisk(1, index)
+        goPlayerTwo()
+    }
+}
+
+function addDisk(player, index) {
+    var col = gameBoard[index]
+    for (let disk in col) {
+        if (disk == 0) {
+            col.push(player)
+            col.splice(0, 1)
+            gameBoard[index] = col
+            render()
+        }
+    }
+}
+
+function colAvailable(index) {
+    return gameBoard[index].filter((disk) => { return disk == 0}).length > 0
 }
 
 function render() {
-    var el = '<div>'
-    for (var col of initailBoard) {
+    document.getElementById('app').innerHTML = ''
+    var el = '<div class="board-col"><br />'
+    for (var [index, col] of gameBoard.entries()) {
         for (var row of col) {
-            el += col[row] + '</div>'
+            el += '<div class="board-cell" onclick="goPlayerOne('+ index + ')">  ' + col[row] + '  </div>'
         }
+        el += '</div>'
     }
     document.getElementById('app').innerHTML += el
 }
